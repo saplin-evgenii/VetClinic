8d138c86c225f7dfb242409a7e6592106d663c17
Evgenii Saplin <evgenii.saplin@vistar.su>
2016-11-03 14:20:59 +0300

    fix: PetController method return type to generic

--- Src/src/main/java/org/seuge/vetclinic/controllers/rest/PetApiController.java
+++ Src/src/main/java/org/seuge/vetclinic/controllers/rest/PetApiController.java
@@ -23,7 +23,7 @@ public abstract class PetApiController<PetType extends Pet, PetDtoType extends P
     }
 
     @Override
-    protected Pet dtoToEntity(PetDTO o) {
+    protected PetType dtoToEntity(PetDTO o) {
         return null;
     }
 }
