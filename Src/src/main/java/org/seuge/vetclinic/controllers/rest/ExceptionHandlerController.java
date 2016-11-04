ce920412923383c36d70b70d034b0a8c9375fc27
Evgenii Saplin <evgenii.saplin@vistar.su>
2016-11-04 17:16:45 +0300

    MVC exception skeleton added

---
+++ Src/src/main/java/org/seuge/vetclinic/controllers/rest/ExceptionHandlerController.java
@@ -0,0 +1,11 @@
+package org.seuge.vetclinic.controllers.rest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * @author Seuge
+ * @since 1.0
+ */
+@ControllerAdvice
+public class ExceptionHandlerController {
+}
